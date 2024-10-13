942319614d30191f0f0c4497aa0106bc7a4e55ee
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-13 08:30:11 +0200

    feat(app): update port binding

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port} 🚀`);
+  });
 }
+
 bootstrap();
