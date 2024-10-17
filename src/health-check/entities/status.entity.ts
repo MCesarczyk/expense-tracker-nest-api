d5680f09f6e33a6848f9442774f0ca46d2e48911
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-17 09:28:17 +0200

    feat(health): update types

--- src/health-check/entities/status.entity.ts
+++ src/health-check/entities/status.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class StatusDto {
+export class Status {
   @ApiProperty({
     example: 'ok',
     description: 'Returns current status of backend application',
