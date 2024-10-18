6664e1cfe2d33c1239c4a3b50cd66ecfd6f3c1d4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-18 09:10:15 +0200

    revert(build): add and configure husky and commitlint
    This reverts commit eb756c28d801a1efec2c272a0d72413f3496e0eb.

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '{apps,libs}/**/*.{ts,js,html,json,scss,css,md}': [
-    'pnpm lint --uncommitted --fix true',
-  ],
-  '*.{ts,js,html,json,scss,css,md,yaml,yml}': [
-    'pnpm format --base=main --head=HEAD',
-  ],
-};
