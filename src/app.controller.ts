611d665522b3b11c30bda4ddb05c08105b0d6cec
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-26 11:20:45 +0200

    fix(app): update redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,12 +8,6 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  @Redirect('api')
-  getRoot(): void {
-    return;
-  }
-
-  @Get('api')
   getHello(): string {
     return this.appService.getHello();
   }
